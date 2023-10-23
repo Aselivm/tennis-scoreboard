f03729a086f4b928412935859628a7e9930f0c2f
Stepan Primshits <Stepan Primshits>
2023-10-23 23:18:16 +0300

    init

--- src/main/java/org/primshic/stepan/controller/BaseServlet.java
+++ src/main/java/org/primshic/stepan/controller/BaseServlet.java
@@ -1,6 +1,19 @@
 package org.primshic.stepan.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public abstract class BaseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 }
