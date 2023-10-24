fd59401ae36767411f4ca6058630206015c02640
Stepan Primshits <Stepan Primshits>
2023-10-24 20:52:40 +0300

    hibernate add

--- src/main/java/org/primshic/stepan/model/Match.java
+++ src/main/java/org/primshic/stepan/model/Match.java
@@ -1,4 +1,33 @@
 package org.primshic.stepan.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Match")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
+
+    @Id
+    @Column(name="ID")
+    private Integer id;
+
+    @Column(name="Player1")
+    private Player player1;
+
+    @Column(name="Player2")
+    private Player player2;
+
+    @Column(name="Winner")
+    private Player winner;
+
 }
--- src/main/java/org/primshic/stepan/model/Player.java
+++ src/main/java/org/primshic/stepan/model/Player.java
@@ -1,4 +1,25 @@
 package org.primshic.stepan.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Player")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Player {
+    @Id
+    @Column(name="ID")
+    private Integer id;
+
+    @Column(name="Name")
+    private String name;
 }
