d42aa854ef9e814ae87a782ceef0644694873f40
Stepan Primshits <Stepan Primshits>
2023-11-05 21:35:13 +0300

    fix filter bug

--- src/main/java/org/primshic/stepan/filter/HeaderFilter.java
+++ src/main/java/org/primshic/stepan/filter/HeaderFilter.java
@@ -4,13 +4,21 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/")
+@WebFilter(urlPatterns = "/*")
 public class HeaderFilter implements Filter {
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletResponse.setContentType("text/html;charset=UTF-8");
+        servletResponse.setContentType("text/html");
         servletRequest.setCharacterEncoding("UTF-8");
         servletResponse.setCharacterEncoding("UTF-8");
         filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
     }
 }
